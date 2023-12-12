841a1a2d262256db395e6f31e8b75805294a7bda
cbarb15 <cbarb15@users.noreply.github.com>
2023-12-11 19:31:13 -0800

    Add files via upload
    Profiles, application, and startup files

---
+++ Application/myThread.c
@@ -0,0 +1,63 @@
+#include <stdint.h>
+#include <stddef.h>
+
+#include <xdc/std.h>
+#include <xdc/runtime/Error.h>
+
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Semaphore.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Event.h>
+#include <ti/sysbios/knl/Clock.h>
+#include <util.h>
+
+/* Driver Header files */
+#include <ti/drivers/GPIO.h>
+#include <ti/display/Display.h>
+#include <ti/drivers/ADC.h>
+
+/* Example/Board Header files */
+#include "Board.h"
+#include "myData.h"
+
+//Task_Struct myTask;
+//#define THREADSTACKSIZE    1024
+//uint8_t myTaskStack[THREADSTACKSIZE];
+//
+//uint16_t adcValue;
+//#define SBP_PERIODIC_EVT_PERIOD   5000
+//#define POST_PARAM_EVT         Event_Id_00
+//
+//extern Display_Handle dispHandle;
+//Clock_Struct clkStruct;
+//Clock_Handle clkHandle;
+//Semaphore_Handle adcSem;
+//
+//
+//void clock_Handler(UArg arg) {
+//    Semaphore_post(adcSem);
+//}
+//
+//
+///********** myThread **********/
+//Task_FuncPtr *myThread(void *arg0) {
+//
+//
+//
+//    while (1) {
+//
+//    }
+//}
+//
+///********** myThread_create **********/
+//void myThread_create(void) {
+//    Task_Params taskParams;
+//
+//    // Configure task
+//    Task_Params_init(&taskParams);
+//    taskParams.stack = myTaskStack;
+//    taskParams.stackSize = THREADSTACKSIZE;
+//    taskParams.priority = 1;
+//
+//   Task_construct(&myTask, myThread, &taskParams, Error_IGNORE);
+//}
